f20fc41de8e43586d88fb311c636df000f544c2a
BI Yue <biyue111@126.com>
2018-03-12 14:57:57 +0800

    Update VM2_server.js

--- Network_connections/VM2_server.js
+++ Network_connections/VM2_server.js
@@ -4,13 +4,19 @@ var http = require('http');
 
 http.createServer(function (request, response) {
 
+    
+    var fs = require("fs");
+
+    var data = fs.readFileSync('input.txt');
+
+    console.log("File Read" + data.toString());
     // Send HTTP header 
     // HTTP state value: 200 : OK
     // Content type: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // Send response "Hello World"
-    response.end('Hello World\n');
+    response.end(data.toString());
 }).listen(8888);
 
 console.log('Server running at http://127.0.0.1:8888/');

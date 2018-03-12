5557f5e3f2b336aaf94bced5f379961f8516b2e5
BI Yue <biyue111@126.com>
2018-03-12 14:42:18 +0800

    Update VM2_server.js

--- Network_connections/VM2_server.js
+++ Network_connections/VM2_server.js
@@ -1,15 +1,16 @@
+//VM2 puts "Hello world" to port 8888
+
 var http = require('http');
 
 http.createServer(function (request, response) {
 
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
+    // Send HTTP header 
+    // HTTP state value: 200 : OK
+    // Content type: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    // 发送响应数据 "Hello World"
+    // Send response "Hello World"
     response.end('Hello World\n');
 }).listen(8888);
 
-// 终端打印如下信息
 console.log('Server running at http://127.0.0.1:8888/');

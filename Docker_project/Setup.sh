1143f0668745f3276c440001bf28e6e89ee4e78c
BI Yue <biyue111@126.com>
2018-03-14 21:28:44 +0800

    Update Setup.sh

--- Docker_project/Setup.sh
+++ Docker_project/Setup.sh
@@ -1,7 +1,7 @@
 docker image build -t vm2 ./VM1
 docker image build -t vm1 ./VM2
 
-docker network create --subnet=192.168.56.0/8 internal
+docker network create --subnet=10.0.2.0/24 internal
 
-docker container run -d --net=internal --ip 192.168.56.102 vm2
-docker container run -d --net=internal --ip 192.168.56.103 -p 8888:8888 vm1
+docker container run -d --net=internal --ip 10.0.2.102 vm2
+docker container run -d --net=internal --ip 10.0.2.103 -p 8888:8888 vm1
